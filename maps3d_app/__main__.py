8b5652b9e21ed5f9eaf893d6b82364e5216ff4bf
T0ny3D <142974318+T0ny3D@users.noreply.github.com>
2026-03-01 09:21:57 +0100

    Refactor main entry point to use main function

--- maps3d_app/__main__.py
+++ maps3d_app/__main__.py
@@ -1,4 +1,8 @@
-from .main import run
+from __future__ import annotations
+
+def main() -> None:
+    from maps3d_app.main import main as run_app
+    run_app()
 
 if __name__ == "__main__":
-    run()
+    main()
