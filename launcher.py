886113fb92ea0f1e2c537fecba48c541e147ad27
T0ny3D <142974318+T0ny3D@users.noreply.github.com>
2026-03-01 09:38:59 +0100

    Update launcher.py

--- launcher.py
+++ launcher.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
 def main() -> None:
-    from maps3d_app.__main__ import main as app_main
-    app_main()
+    from maps3d_app.main import main as run_app
+    run_app()
 
 if __name__ == "__main__":
     main()
